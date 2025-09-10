80f059b24a1c6dd15d15b532aaa48009585a7014
RulsOfficial <130804534+RulsOfficial@users.noreply.github.com>
2025-09-10 22:48:28 +0000

    Fixed the name of some killers

--- js/update-perks.js
+++ js/update-perks.js
@@ -3,34 +3,45 @@ const path = require('path');
 const { JSDOM } = require('jsdom');
 const axios = require("axios").default;
 
+// 🔹 Mapeo de reemplazos de personajes
+const killerNameMap = {
+	"Frank, Julie, Susie, Joey": "Legion",
+	"Danny Johnson": "Ghost Face",
+	"Nemesis T-Type": "Nemesis",
+	"Kenneth Chase": "Clown",
+	"Charles Lee Ray": "The Good Guy"
+};
+
 // 🔹 Función que parsea perks desde la wiki
 async function parsePerks(url) {
-	const stuff = await axios.get(url);
-	const dom = new JSDOM(stuff.data);
+	const response = await axios.get(url);
+	const dom = new JSDOM(response.data);
 	const { document } = dom.window;
 
 	// Extraer todas las filas de la tabla
-	let perks = [...document.querySelector("tbody").children].map((x) => {
+	let perks = [...document.querySelector("tbody").children].map((row) => {
 		// Quitar iconitos de enlaces internos
-		x.children[2].querySelectorAll(".iconLink").forEach((y) => y.remove());
+		row.children[2].querySelectorAll(".iconLink").forEach(el => el.remove());
 
-		// Imagen del perk
-		const imageElement = x.children[0].querySelector("img");
+		const imageElement = row.children[0].querySelector("img");
 		const imageUrl = imageElement?.src.substring(0, imageElement.src.lastIndexOf("/")).replace("/thumb", "");
+
+		let characterName = row.children[3].querySelector("a")?.title || "";
+
+		// Reemplazar nombre si está en el map
+		if (killerNameMap[characterName]) {
+			characterName = killerNameMap[characterName];
+		}
 
 		return {
 			perkImage: imageElement ? "https://deadbydaylight.wiki.gg" + imageUrl : "",
 			perkName: imageElement?.alt || "",
-			// Descripción codificada en URI
-			description: encodeURI(
-				x.children[2].innerHTML.replaceAll("/wiki/", "https://deadbydaylight.wiki.gg/wiki/")
-			),
-			character: x.children[3].querySelector("a")?.title || "",
-			characterImage: x.children[3].querySelector("img")?.src || ""
+			description: encodeURI(row.children[2].innerHTML.replaceAll("/wiki/", "https://deadbydaylight.wiki.gg/wiki/")),
+			character: characterName,
+			characterImage: row.children[3].querySelector("img")?.src || ""
 		};
 	});
 
-	// Ordenar perks alfabéticamente
 	perks.sort((a, b) => a.perkName.localeCompare(b.perkName, 'en'));
 	return perks;
 }
@@ -40,19 +51,12 @@ async function parsePerks(url) {
 	try {
 		console.log("⏳ Descargando perks desde la wiki...");
 
-		// Parsear perks de killers y survivors
 		const killerPerks = await parsePerks("https://deadbydaylight.wiki.gg/wiki/Killer_Perks");
 		const survivorPerks = await parsePerks("https://deadbydaylight.wiki.gg/wiki/Survivor_Perks");
 
-		// Carpeta data en el root del repo (aunque el script esté en otra carpeta)
 		const dataDir = path.join(__dirname, "..", "data");
+		if (!fs.existsSync(dataDir)) fs.mkdirSync(dataDir, { recursive: true });
 
-		// Crear la carpeta si no existe
-		if (!fs.existsSync(dataDir)) {
-			fs.mkdirSync(dataDir, { recursive: true });
-		}
-
-		// Guardar los archivos y sobrescribir si existen
 		fs.writeFileSync(path.join(dataDir, "killer-perks.json"), JSON.stringify(killerPerks, null, '\t'));
 		fs.writeFileSync(path.join(dataDir, "survivor-perks.json"), JSON.stringify(survivorPerks, null, '\t'));
 
