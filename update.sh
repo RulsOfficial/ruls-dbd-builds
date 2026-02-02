df92c56a1e1123e1df3492202637369a29e01576
RulsOfficial <130804534+RulsOfficial@users.noreply.github.com>
2026-02-02 18:24:58 +0000

    Autoupdate

--- update.sh
+++ update.sh
@@ -1,18 +1,9 @@
-# 1️⃣ Instalar dependencias
+#!/bin/sh
+set -e
 npm install axios jsdom
-
-# 2️⃣ Ejecutar el script de actualización
-cd js
-node update-perks.js
-cd ..
-
-# 3️⃣ Limpiar node_modules y package.json
-rm -rf node_modules
-rm -f package.json package-lock.json
-
-# 4️⃣ Commit y push a Git
+(cd js && node update-perks.js)
+rm -rf node_modules package.json package-lock.json
 git add .
 git commit -m "Autoupdate"
 git push
-
 echo "✅ Script finalizado y cambios subidos a Git"
